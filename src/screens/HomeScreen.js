77578270300a9ec5abef20287eda566c2cda265c
Loveleen Kaur <loveleen.nancy@gmail.com>
2020-04-18 00:14:19 +0530

    Added SquareScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -25,6 +25,10 @@ const HomeScreen = ({navigation}) => {
       <Button
         onPress={() => navigation.navigate('Color')}
         title="Go to Color Demo"/>
+
+      <Button
+        onPress={() => navigation.navigate('Square')}
+        title="Go to Square Demo"/>  
       
     </View>
 };
